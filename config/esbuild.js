a7ce537a76b0009be6a679682baf12beede38c37
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-08-25 15:33:57 +0330

    feat: add en lang prefix

--- config/esbuild.js
+++ config/esbuild.js
@@ -1,26 +1,51 @@
-const { build } = require("esbuild");
+const { build, transform } = require("esbuild");
 const { env } = require("process");
 
 const debugMode = env.NODE_ENV !== "production";
-async function esbuild() {
-  await build({
-    entryPoints: [`site/_js/main.ts`],
-    outdir: "dist/",
-    logLevel: "info",
-    platform: "browser",
-    target: "es2018",
-    format: "esm",
-    conditions: debugMode ? ["development"] : undefined,
-    minify: true,
-    treeShaking: true,
-    sourcemap: true,
-    sourcesContent: debugMode,
-    bundle: true,
-    splitting: true,
-    charset: "utf8",
-    legalComments: "none",
-    metafile: true,
-  });
+async function esbuildBuild() {
+  try {
+    await build({
+      entryPoints: [`site/_js/main.ts`],
+      outdir: "dist/",
+      logLevel: "info",
+      platform: "browser",
+      target: "es2018",
+      format: "esm",
+      minify: true,
+      treeShaking: true,
+      sourcemap: true,
+      sourcesContent: debugMode,
+      bundle: true,
+      charset: "utf8",
+      legalComments: "none",
+      // splitting: true,
+    });
+  } catch (err) {
+    console.error('esbuildProcess Error: ', err);
+  }
 }
 
-module.exports = { esbuild };
+async function esbuildTransform(content) {
+  try {
+    const result = await transform(content, {
+      logLevel: "info",
+      platform: "browser",
+      target: "es2018",
+      format: "esm",
+      minify: true,
+      treeShaking: true,
+      sourcemap: true,
+      // bundle: true,
+      // splitting: true,
+      charset: "utf8",
+      legalComments: "none",
+    });
+
+    return result.code;
+  } catch (err) {
+    console.error('esbuildTransform Error: ', err);
+    return content;
+  }
+}
+
+module.exports = { esbuildBuild, esbuildTransform };
