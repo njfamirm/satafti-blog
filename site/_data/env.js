d4f460af838102c63cb8db6fb7c495a17b67dd2d
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-09-05 20:59:13 +0330

    feat: google analytics section

--- site/_data/env.js
+++ site/_data/env.js
@@ -2,6 +2,7 @@ const env = {
   // ...process.env,
   siteUrl: process.env.siteUrl ?? '',
   noIndex: process.env.noIndex === '1',
+  production: process.env.NODE_ENV === 'production',
 };
 
 console.log(JSON.stringify(env));
