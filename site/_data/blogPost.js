8c28bdd7e2472a9dd56aa92bbcb70228001ccbb6
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-10-02 10:05:58 +0330

    feat(data): load blog post from cms

---
+++ site/_data/blogPost.js
@@ -0,0 +1,22 @@
+const fetch = require('@11ty/eleventy-fetch');
+
+const apiToken = process.env.cmsApiToken;
+const cmsDomain = process.env.cmsDomain ?? 'https://admin.njfamirm.ir';
+const url = cmsDomain + '/api/blog-posts?populate=*';
+
+async function list() {
+  const response = await fetch(url, {
+    directory: '.cache',
+    duration: '1s',
+    type: 'json',
+    fetchOptions: {
+      headers: {
+        Authorization: `Bearer ${apiToken}`,
+      },
+    },
+  });
+
+  return response.data;
+}
+
+module.exports = list;
